e576498063135ea305c31bcdcec83be87bd0586e
alexandersverdlin <asverdlin@nes.ru>
2021-06-11 12:41:02 +0300

    spoti

--- main.py
+++ main.py
@@ -61,3 +61,58 @@ with st.echo(code_location="above"):
     coefs.plot.bar(color = 'orange')
     fig = plt.plot()
     st.pyplot(fig)
+
+    # df_full = pd.read_csv("C:/Users/asverdlin/Downloads/tracks.csv")
+    # df_feats = df_full.sort_values(by = 'popularity', ascending = False)['artists']
+    # df_feats.to_csv('C:/Users/asverdlin/Downloads/df_feats.csv')
+
+    # artists_full = pd.read_csv("C:/Users/asverdlin/Downloads/artists.csv")
+    # artists_lite = artists_full.sort_values(by = 'followers', ascending = False)[0:1000]
+    # artists_lite.to_csv('C:/Users/asverdlin/Downloads/artists_lite.csv')
+
+    artists = pd.read_csv("artists_lite.csv")
+    df_feats = pd.read_csv("df_feats.csv")
+
+    most_followed_artists = artists.sort_values(by='followers', ascending=False)[0:50]
+
+    G = nx.Graph()
+
+    vertices = list(most_followed_artists['name'])
+    vertices
+
+    G.add_nodes_from(vertices)
+
+    feats = set()
+    for star in vertices:
+        for song in df_feats['artists']:
+            if star in song:
+                feats.add(song)
+
+    true_feats = set()
+    for feat in feats:
+        for star in vertices:
+            vertices
+            for star2 in vertices:
+                if star != star2:
+                    if star in feat and star2 in feat:
+                        true_feats.add(feat)
+
+    feats_list = [feat.replace("'", "").replace('[', '').replace(']', '').split(', ') for feat in true_feats]
+
+    for feat in feats_list:
+        for artist in feat:
+            if artist not in vertices:
+                feat.remove(artist)
+
+    for el in feats_list:
+        if len(el) > 2 or len(el) < 2:
+            feats_list.remove(el)
+
+    feats_list_tuples = [tuple(el) for el in feats_list]
+    feats_list_tuples
+
+    G.add_edges_from(feats_list_tuples)
+
+    net = Network(width='1000px', notebook=True)
+    net.from_nx(G)
+    net.show("feats.html")
